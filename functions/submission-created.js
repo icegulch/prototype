6cd54d40159b894d8972c959f2bbafa9db0a80c9
Steven Bailey <stevenbailey@Stevens-Air.attlocal.net>
2023-10-10 08:56:08 -0400

    serverless functions are black boxes3

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,4 +1,87 @@
+
+const fetch = require('node-fetch');
+
 exports.handler = async event => {
-  const submission = JSON.parse(event.body).payload
-  console.log(submission);
-}
+  try {
+    // Extract form submission data
+    const submissionData = JSON.parse(event.body).payload.data;
+
+    // Prepare the simplified data
+    const simplifiedData = {
+      id: submissionData.id,
+      created_at: submissionData.created_at,
+      url: submissionData.url,
+      title: submissionData.title,
+      message: submissionData.message,
+    };
+
+    // GitHub repository information
+    const repoOwner = 'icegulch';
+    const repoName = 'prototype';
+    const filePath = 'src/_data/posts.json';
+
+    // GitHub Personal Access Token with appropriate permissions
+    const githubToken = `${GITHUB_TOKEN}`;
+
+    // Fetch the existing posts.json content
+    const response = await fetch(
+      `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${githubToken}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch existing content');
+    }
+
+    const existingContent = await response.json();
+
+    // Decode the existing content and add the new data
+    const decodedContent = JSON.parse(
+      Buffer.from(existingContent.content, 'base64').toString('utf-8')
+    );
+
+    decodedContent.push(simplifiedData);
+
+    // Encode the modified content back to base64
+    const updatedContent = Buffer.from(
+      JSON.stringify(decodedContent, null, 2),
+      'utf-8'
+    ).toString('base64');
+
+    // Update the posts.json file on GitHub
+    const updateResponse = await fetch(
+      `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`,
+      {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${githubToken}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          message: 'Update posts.json',
+          content: updatedContent,
+          sha: existingContent.sha,
+        }),
+      }
+    );
+
+    if (!updateResponse.ok) {
+      throw new Error('Failed to update content on GitHub');
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Form submitted and posts.json updated' }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
